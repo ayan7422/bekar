18d49031cc5ccd121a9326ad1f3a4c0035599bc2
ayan7422 <ayanji280@gmail.com>
2025-08-01 06:45:01 +0500

    Update pair.js

--- Priyansh/commands/pair.js
+++ Priyansh/commands/pair.js
@@ -1,78 +1,23 @@
-module.exports.config = {
-  name: "pair",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "Faheem Akhtar",
-  description: "pairing",
-  commandCategory: "Love",
-  usages: "pair",
-  cooldowns: 10,
-};
-module.exports.run = async function ({ api, event, Threads, Users }) {
-  const axios = global.nodemodule["axios"];
-  const fs = global.nodemodule["fs-extra"];
+ // Random partner
+ let threadInfo = await api.getThreadInfo(threadID);
+ let participants = threadInfo.participantIDs.filter(id => id !== senderID);
+ let partnerID = participants[Math.floor(Math.random() * participants.length)];
+ let partnerInfo = await api.getUserInfo(partnerID);
+ let partnerName = partnerInfo[partnerID].name;
 
-  var { participantIDs } = (await Threads.getData(event.threadID)).threadInfo;
-  var tle = Math.floor(Math.random() * 101);
-  var namee = (await Users.getData(event.senderID)).name;
-  const botID = api.getCurrentUserID();
-  const listUserID = event.participantIDs.filter(
-    (ID) => ID != botID && ID != event.senderID
-  );
-  var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-  var name = (await Users.getData(id)).name;
-  var arraytag = [];
-  const gifCute = [
-    "https://i.pinimg.com/originals/42/9a/89/429a890a39e70d522d52c7e52bce8535.gif",
-    "https://i.imgur.com/HvPID5q.gif",
-    "https://i.pinimg.com/originals/9c/94/78/9c9478bb26b2160733ce0c10a0e10d10.gif",
-    "https://i.pinimg.com/originals/9d/0d/38/9d0d38c79b9fcf05f3ed71697039d27a.gif",
-    "https://i.imgur.com/BWji8Em.gif",
-    "https://i.imgur.com/ubJ31Mz.gif",
-  ];
-  arraytag.push({ id: event.senderID, tag: namee });
-  arraytag.push({ id: id, tag: name });
+ // Mentions
+ let mentions = [
+ { id: senderID, tag: senderName },
+ { id: partnerID, tag: partnerName }
+ ];
 
-  let Avatar = (
-    await axios.get(
-      `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(__dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8"));
-
-  let gifLove = (
-    await axios.get(gifCute[Math.floor(Math.random() * gifCute.length)], {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(
-    __dirname + "/cache/giflove.png",
-    Buffer.from(gifLove, "utf-8")
-  );
-
-  let Avatar2 = (
-    await axios.get(
-      `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(
-    __dirname + "/cache/avt2.png",
-    Buffer.from(Avatar2, "utf-8")
-  );
-
-  var imglove = [];
-
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
-  var msg = {
-    body: `🅢𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋 🅟𝐀𝐈𝐑𝐈𝐍𝐆
-𝐇𝐎𝐏𝐄 𝐘𝐎𝐔 𝐁𝐎𝐓𝐇 𝐖𝐈𝐋𝐋 𝐒𝐓𝐎𝐏 𝐅𝐋𝐈𝐑𝐓𝐈𝐍𝐆 ⊂◉‿◉\n━━━━━━━━━━━━━━━━━━ ${namee} 💓 ${name}\n━━━━━━━━━━━━━━━━━━\n➥ 𝐃𝐎𝐔𝐁𝐋𝐄 𝐑𝐀𝐓𝐈𝐎: ${tle}%\n━━━━━━━━━━━━━━━━━━\n𝙊𝙬𝙣𝙚𝙧 Åyãñ Hûñ yãr`,
-    mentions: arraytag,
-    attachment: imglove,
-  };
-  return api.sendMessage(msg, event.threadID, event.messageID);
+ // Generate and send image
+ let one = senderID, two = partnerID;
+ return makeImage({ one, two }).then(path => {
+ api.sendMessage({
+ body: `🥰 Successful Pairing!\n💌 Wishing you two a lifetime of unexpected happiness – even with a ${matchRate} match!\n💕 Compatibility Score: ${matchRate}\nUnlikely but Unstoppable: [${senderName} + ${partnerName}]👨‍❤️‍👨`,
+ mentions,
+ attachment: fs.createReadStream(path)
+ }, threadID, () => fs.unlinkSync(path), messageID);
+ });
 };
