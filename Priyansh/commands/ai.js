6a731dc919835747144abaf6cc95f84758fc6616
ayan7422 <ayanji280@gmail.com>
2025-08-01 10:51:12 +0500

    Update ai.js

--- Priyansh/commands/ai.js
+++ Priyansh/commands/ai.js
@@ -1,7 +1,7 @@
 
   const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const request = require('ai');
 const axios = require('axios');
 
 const app = express();
